f736106f1d2f755137538d6104cd1c6e4ba29c18
MehmetMertDenizman <amorylorch@gmail.com>
2022-07-10 14:20:52 +0300

    JS
    Dolphins

--- boring/js.js
+++ boring/js.js
@@ -1,7 +1,8 @@
 'use strict';
 let tryer = 0;
 let sum = 0;
-while (tryer <= 2) {
+while (tryer <= 2)
+{
     tryer++;
     let dolphinsPoint = prompt(parseInt("ENTER"), "DOLPHINS");
     let dolphinsPointAvg = parseInt(dolphinsPoint);
@@ -12,3 +13,9 @@ while (tryer <= 2) {
 const calcAverage = calcAverage => sum / 3;
 console.log(calcAverage(sum));
 
+let tryer2 = 0;
+let sum2 = 0;
+
+while (tryer2 <= 2)
+
+}
